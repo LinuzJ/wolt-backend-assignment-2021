f689dfa5722e0fd85d40da0b4fe3af59637246dc
LinuzJ <linusjern00@gmail.com>
2021-01-31 13:58:01 +0200

    udatde

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 import requests
 import json
 from distance import get_distance
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/discovery')
 def api():
+    # getting the user data
+    lat_user  = float(request.args.get('lat', None))
+    lon_user  = float(request.args.get('lon', None))
+    
+    
     temp = requests.get("https://raw.githubusercontent.com/woltapp/summer2021-internship/main/restaurants.json")
-    print(get_distance(60.171987, 24.941244, 60.156621, 24.935637))
+    print(get_distance(lat_user, lon_user, 60.156621, 24.935637))
     return json.loads(temp.content)
 
 if __name__ == '__main__':
