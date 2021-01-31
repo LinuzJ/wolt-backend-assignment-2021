a0a1d89c3208b8d3aa8e315511806b03993cf81b
LinuzJ <linusjern00@gmail.com>
2021-01-31 15:07:06 +0200

    correct output

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import json
 from distance import get_distance
 from helpers import sort_by_distance, sort_by_popularity, sort_by_date
+
 app = Flask(__name__)
 
 @app.route('/discovery')
@@ -11,11 +12,30 @@ def api():
     lat_user  = float(request.args.get('lat', None))
     lon_user  = float(request.args.get('lon', None))
     
-    temp = requests.get("https://raw.githubusercontent.com/woltapp/summer2021-internship/main/restaurants.json")
-    restaurants = json.loads(temp.content)["restaurants"]
+    restaurant_data = requests.get("https://raw.githubusercontent.com/woltapp/summer2021-internship/main/restaurants.json")
+    restaurants = json.loads(restaurant_data.content)["restaurants"]
 
-    print(sort_by_popularity(sort_by_distance(restaurants, lat_user, lon_user)))
-    return {"hej":sort_by_date(sort_by_distance(restaurants, lat_user, lon_user))}
+    sorted_restaurants = sort_by_distance(restaurants, lat_user, lon_user)
+    
+    return_data = {
+        "sections": [
+            {
+                "title": "Popular Restaurants",
+                "restaurants": sort_by_popularity(sorted_restaurants)[:10]
+            },
+            {
+                "title": "New Restaurants",
+                "restaurants": sort_by_date(sorted_restaurants)[:10]
+            },
+            {
+                "title": "Nearby Restaurants",
+                "restaurants": sorted_restaurants[:10]
+            }
+
+        ]
+    }
+
+    return return_data
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
