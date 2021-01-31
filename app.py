7f002024da3fd9e49fd3f76a2ff078911213e1f2
LinuzJ <linusjern00@gmail.com>
2021-01-31 14:59:13 +0200

    date sorted works

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, request
 import requests
 import json
 from distance import get_distance
-from helpers import sort_by_distance
-
+from helpers import sort_by_distance, sort_by_popularity, sort_by_date
 app = Flask(__name__)
 
 @app.route('/discovery')
@@ -15,8 +14,8 @@ def api():
     temp = requests.get("https://raw.githubusercontent.com/woltapp/summer2021-internship/main/restaurants.json")
     restaurants = json.loads(temp.content)["restaurants"]
 
-
-    return {"hej":sort_by_distance(restaurants, lat_user, lon_user)}
+    print(sort_by_popularity(sort_by_distance(restaurants, lat_user, lon_user)))
+    return {"hej":sort_by_date(sort_by_distance(restaurants, lat_user, lon_user))}
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 from distance import get_distance
+from datetime import datetime, date
 
 
 def sort_by_distance(data, lat, lon):
@@ -7,7 +8,28 @@ def sort_by_distance(data, lat, lon):
         if get_distance(restaurant["location"][1], restaurant["location"][0], lat, lon) < 1.5:
             list_without_far_away.append(restaurant)
     
-    newList = sorted(list_without_far_away, key=lambda k: get_distance(k["location"][1], k["location"][0], lat, lon))
-    for r in newList:
-        print(get_distance(r["location"][1], r["location"][0], lat, lon))
+    list_with_distance = sorted(list_without_far_away, key=lambda k: get_distance(k["location"][1], k["location"][0], lat, lon))
+    
+    newList = sorted(list_with_distance, key=lambda k: k["online"], reverse=True)
+
     return newList
+
+
+
+
+def sort_by_popularity(data):
+    return sorted(data, key=lambda k: k["popularity"], reverse=True)
+
+def d_months(date1, date2):
+    return (date1.year - date2.year) * 12 + date1.month - date2.month   
+
+def sort_by_date(data):
+    # first we filter out all older than 4 months
+    date_format = '%Y-%m-%d'
+    today = date.today()
+
+    filtered = filter(lambda restaurant: d_months(today, datetime.strptime(restaurant['launch_date'], date_format)) < 4, data)
+    
+    sorted_list = sorted(filtered, key=lambda restaurant: restaurant['launch_date'], reverse=True)
+
+    return sorted_list
